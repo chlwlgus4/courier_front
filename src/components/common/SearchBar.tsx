b6c68bb6f730f24fd7b0d6e04d06736041e0bc1d
chlwlgus4 <chlwlgus695@gmail.net>
2025-05-13 16:39:35 +0900

    css 변경

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -11,7 +11,7 @@ export function SearchBar() {
           <input
             type="text"
             placeholder="검색"
-            className="w-full py-3 pl-4 pr-12 rounded-full bg-white border-2 border-teal-400 text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-300"
+            className="w-full py-2 pl-4 pr-12 rounded-full bg-white border-2 border-teal-400 text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-teal-300"
           />
           <AiOutlineSearch className="absolute right-4 top-1/2 transform -translate-y-1/2 text-teal-500 text-xl" />
         </div>
